3e16d9cedfff5d3c6aa69b3bfa428053ecfbd383
Antonio Cardace <acardace@redhat.com>
2022-10-21 14:16:57 +0200

    Create pods with AllowPrivilegeEscalation = false
    This is a requirement to create pods in a restricted (PSA)
    namespace
    
    Signed-off-by: Antonio Cardace <acardace@redhat.com>

--- pkg/virt-controller/services/rendercontainer.go
+++ pkg/virt-controller/services/rendercontainer.go
@@ -160,15 +160,17 @@ func xdgEnvironmentVariables() []k8sv1.EnvVar {
 func securityContext(userId int64, privileged bool, requiredCapabilities *k8sv1.Capabilities) *k8sv1.SecurityContext {
 	isNonRoot := userId != 0
 	context := &k8sv1.SecurityContext{
-		RunAsUser:    &userId,
-		RunAsNonRoot: &isNonRoot,
-		Privileged:   &privileged,
-		Capabilities: requiredCapabilities,
+		AllowPrivilegeEscalation: &privileged,
+		RunAsUser:                &userId,
+		RunAsNonRoot:             &isNonRoot,
+		Privileged:               &privileged,
+		Capabilities:             requiredCapabilities,
 	}
 
 	if isNonRoot {
 		context.RunAsGroup = &userId
 	}
+
 	return context
 }
 
